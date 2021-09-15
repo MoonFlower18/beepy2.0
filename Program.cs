719f496d2643e2ecea625da9b9f72f89e165eae4
Юля <rusal@bk.ru>
2021-09-15 18:04:27 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace beepy2
 {
@@ -7,6 +8,52 @@ namespace beepy2
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      //Здравствуй
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(523, 300);
+      Console.Beep(493, 300);
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Thread.Sleep(700);
+
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(523, 300);
+      Console.Beep(493, 300);
+      Console.Beep(523, 300);
+      Console.Beep(659, 300);
+      Thread.Sleep(700);
+
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(523, 300);
+      Console.Beep(493, 300);
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Thread.Sleep(700);
+
+
+
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(523, 300);
+      Console.Beep(493, 300);
+      Console.Beep(523, 300);
+      Console.Beep(659, 300);
+      Thread.Sleep(700);
+      //Эх
+      Console.Beep(659, 300);
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Console.Beep(349, 500);
+      Console.Beep(440, 500);
+      Console.Beep(523, 500);
+      Thread.Sleep(700);
     }
   }
 }
