c311fbd005b180d4b14a06ee94a322c58b8548a0
juravleva.yuliya202@gmail.com <juravleva.yuliya202@gmail.com>
2021-09-17 23:56:21 +0400

    Оформление 1 части

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace beepy2
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      //Здравствуй
+      Console.WriteLine("Мальчикам посвящается!");
+      Thread.Sleep(400);
+      //Здравствуй небо в облаках
       Console.Beep(587, 300);
       Console.Beep(587, 300);
       Console.Beep(587, 300);
@@ -16,8 +17,8 @@ namespace beepy2
       Console.Beep(493, 300);
       Console.Beep(523, 300);
       Console.Beep(440, 300);
-      Thread.Sleep(700);
-
+      Thread.Sleep(400);
+      //Здравствуй юность в сапогах      
       Console.Beep(587, 300);
       Console.Beep(587, 300);
       Console.Beep(587, 300);
@@ -25,8 +26,8 @@ namespace beepy2
       Console.Beep(493, 300);
       Console.Beep(523, 300);
       Console.Beep(659, 300);
-      Thread.Sleep(700);
-
+      Thread.Sleep(400);
+      //Пропади моя тоска  
       Console.Beep(587, 300);
       Console.Beep(587, 300);
       Console.Beep(587, 300);
@@ -34,10 +35,8 @@ namespace beepy2
       Console.Beep(493, 300);
       Console.Beep(523, 300);
       Console.Beep(440, 300);
-      Thread.Sleep(700);
-
-
-
+      Thread.Sleep(400);
+      //Вот он я, привет войска!      
       Console.Beep(587, 300);
       Console.Beep(587, 300);
       Console.Beep(587, 300);
@@ -45,15 +44,62 @@ namespace beepy2
       Console.Beep(493, 300);
       Console.Beep(523, 300);
       Console.Beep(659, 300);
-      Thread.Sleep(700);
-      //Эх
+      Thread.Sleep(100);
+      //Эх рельсы, поезда
       Console.Beep(659, 300);
       Console.Beep(523, 300);
       Console.Beep(440, 300);
       Console.Beep(349, 500);
       Console.Beep(440, 500);
       Console.Beep(523, 500);
-      Thread.Sleep(700);
+      Thread.Sleep(100);
+      //Как я попал сюда?!
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Console.Beep(349, 300);
+      Console.Beep(392, 500);
+      Console.Beep(493, 500);
+      Console.Beep(587, 500);
+      Thread.Sleep(400);
+      //Здесь не то, что на гражданке,
+      Console.Beep(523, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 400);
+      Thread.Sleep(100);  
+      //На какой-нибудь гражданке,
+      Console.Beep(659, 200);
+      Console.Beep(587, 200);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Thread.Sleep(100);
+      //Жизнь снаружи и с изнанки,
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Console.Beep(659, 200);
+      Console.Beep(587, 400);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Thread.Sleep(100);
+      //Сам попробуй изучить.
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Console.Beep(659, 200);
+      Console.Beep(587, 300);
+      Console.Beep(523, 700);
+      Thread.Sleep(200);
     }
   }
 }
