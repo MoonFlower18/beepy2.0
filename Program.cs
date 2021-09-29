f6edc034b2e0f2a746faa86c3c8286e968c69afd
juravleva.yuliya202@gmail.com <juravleva.yuliya202@gmail.com>
2021-09-29 10:33:18 +0300

    +2 часть 1 купплета

--- Program.cs
+++ Program.cs
@@ -121,6 +121,89 @@ namespace beepy2
       Console.Beep(523, 300);
       Console.Beep(440, 400);
       Thread.Sleep(400);
+
+      Console.WriteLine(" ");
+      //Проигрыш
+      Console.Beep(293, 300);
+      Console.Beep(293, 300);
+      Console.Beep(293, 300);
+      Console.Beep(261, 300);
+      Console.Beep(246, 300);
+      Console.Beep(261, 300);
+      Console.Beep(220, 600);
+      Thread.Sleep(600);
+
+      Console.WriteLine("Непросто быть собой,");
+      Console.Beep(659, 300);
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Console.Beep(349, 500);
+      Console.Beep(440, 500);
+      Console.Beep(523, 500);
+      Thread.Sleep(100);
+
+      Console.WriteLine("Когда шагает строй,");
+      Console.Beep(523, 300);
+      Console.Beep(440, 300);
+      Console.Beep(349, 300);
+      Console.Beep(392, 500);
+      Console.Beep(493, 500);
+      Console.Beep(587, 500);
+      Thread.Sleep(400);
+      
+      Console.WriteLine("Только сердце птицей бьётся");
+      Console.Beep(523, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 400);
+      Thread.Sleep(100);
+
+      Console.WriteLine("И ликует, и смеётся,");
+      Console.Beep(659, 200);
+      Console.Beep(587, 200);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 400);
+      Console.Beep(698, 200);
+      Thread.Sleep(100);
+
+      Console.WriteLine("И ему не удаётся");
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Console.Beep(659, 200);
+      Console.Beep(587, 400);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Thread.Sleep(100);
+
+      Console.WriteLine("Под конвоем петь в груди.");
+      Console.Beep(659, 300);
+      Console.Beep(587, 300);
+      Console.Beep(659, 300);
+      Console.Beep(698, 300);
+      Console.Beep(659, 200);
+      Console.Beep(587, 300);
+      Console.Beep(523, 700);
+      Thread.Sleep(800);
+
+      Console.WriteLine("Знать бы, что там впереди...");
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(587, 300);
+      Console.Beep(523, 300);
+      Console.Beep(493, 300);
+      Console.Beep(523, 300);
+      Console.Beep(440, 400);
+      Thread.Sleep(400);
+
       //Проигрыш
       Console.Beep(293, 300);
       Console.Beep(293, 300);
